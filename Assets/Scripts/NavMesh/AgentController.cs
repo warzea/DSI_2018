5e49c1e32115ac08d38e1776e340f0afabfcf7d6
Unknown <davidbourel.29@hotmail.fr>
2018-03-06 22:59:20 +0100

    WIP_ManagerAgent_NePasRecup

--- Assets/Scripts/NavMesh/AgentController.cs
+++ Assets/Scripts/NavMesh/AgentController.cs
@@ -5,31 +5,66 @@ using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
-    public NavMeshAgent[] agents;
+	/// <summary> Public <summary>
+	[Header("-------------------")]
+	[Header("----INFO PLAYER----")]
+	[Header("-------------------")]
 
-    private RaycastHit m_HitInfo = new RaycastHit();
-    private Vector3 posPlayer;
+	[Header("PlayerPref")]
+	public GameObject[] player;
+
+	[Header("Timer Agent Sleep and Info")]
+	public float maxPosPlayer;
+	public float timeLeftAgentLook = 0.5f;
+
+	[Header("------------------")]
+	[Header("----INFO FOCUS----")]
+	[Header("------------------")]
+	public int pourcentLowPV;
+	public int pourcentMaxPV;
+	public int pourcentLead;
+
+
+	/// <summary> Private <summary>
+
+	//Agent
+	private NavMeshAgent[] agents;
+
+	//Timer
+	private float timeAgent = 5;
+
 
     void Start()
     {
         agents = GameObject.FindObjectsOfType<NavMeshAgent>();
+		InitGame ();
     }
+
+	public void InitGame(){
+		int nbAgents = agents.Length;
+
+		float nbLawPv = Mathf.Round(nbAgents * pourcentLowPV/100);
+		float nbMaxPv = Mathf.Round(nbAgents * pourcentMaxPV/100);
+		float nbLead = Mathf.Round(nbAgents * pourcentLead/100);
+		float nbOther = nbAgents - nbLawPv - nbMaxPv - nbLead;
+
+	}
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
-            {
-                posPlayer = m_HitInfo.point;
-
-            }
-
-            for (int i = 0; i < agents.Length; i++)
-            {
-                agents[i].SetDestination(posPlayer);
-            }
-        }
+		timeAgent += Time.deltaTime;
+		if ( timeAgent > timeLeftAgentLook)
+		{
+			for (int i = 0; i < agents.Length; i++)
+			{
+				float distance = Mathf.Abs (Vector3.Distance (agents [i].transform.position, player[0].transform.position));
+				if(distance>maxPosPlayer){
+					float randStopDist = Random.Range (15, 25);
+					agents[i].stoppingDistance = randStopDist;
+					agents[i].SetDestination(player[0].transform.position);
+				}
+			}
+			timeAgent = 0;
+		}
     }
 }
---
+++ Assets/Scripts/NavMesh/AgentInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentInfo : MonoBehaviour {
+
+	private NavMeshAgent agents;
+
+	// Use this for initialization
+	void Start () {
+		agents = transform.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(agents.isStopped){
+			Debug.Log ("ici");
+		}
+	}
+}
