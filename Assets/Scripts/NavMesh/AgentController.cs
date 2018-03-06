c03e689383cc9b92a1f4238d5f2608ea6fe2db1f
Unknown <davidbourel.29@hotmail.fr>
2018-03-06 17:20:53 +0100

    Done_AddNavMesh

---
+++ Assets/Scripts/NavMesh/AgentController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    public NavMeshAgent[] agents;
+
+    private RaycastHit m_HitInfo = new RaycastHit();
+    private Vector3 posPlayer;
+
+    void Start()
+    {
+        agents = GameObject.FindObjectsOfType<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+            {
+                posPlayer = m_HitInfo.point;
+
+            }
+
+            for (int i = 0; i < agents.Length; i++)
+            {
+                agents[i].SetDestination(posPlayer);
+            }
+        }
+    }
+}
