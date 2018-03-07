c9f98764a7e354a00da38965b49308792cd1fdae
Unknown <davidbourel.29@hotmail.fr>
2018-03-07 16:04:59 +0100

    WIP_IA_Movement

--- Assets/Scripts/NavMesh/AgentController.cs
+++ Assets/Scripts/NavMesh/AgentController.cs
@@ -3,75 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class AgentController : MonoBehaviour
 {
-	/// <summary> Public <summary>
-	[Header("-------------------")]
-	[Header("----INFO PLAYER----")]
-	[Header("-------------------")]
+    public enum CibleAgent { lawPlayer, maxPlayer, leadPlayer, randomPlayer, nothing };
+    public enum AgentEtat { deadAgent, aliveAgent };
+    public CibleAgent myFocusEtatAgent;
+    public bool dead = false;
 
-	[Header("PlayerPref")]
-	public GameObject[] player;
-
-	[Header("Timer Agent Sleep and Info")]
-	public float maxPosPlayer;
-	public float timeLeftAgentLook = 0.5f;
-
-	[Header("------------------")]
-	[Header("----INFO FOCUS----")]
-	[Header("------------------")]
-	public int pourcentLowPV;
-	public int pourcentMaxPV;
-	public int pourcentLead;
+    public GameObject myFocusPlayer;
+    private AgentsManager agentsManager;
+    private NavMeshAgent navAgent;
 
 
-	/// <summary> Private <summary>
-
-	//Agent
-	private NavMeshAgent[] agents;
-
-	//Timer
-	private float timeAgent = 5;
-
-
-    void Start()
+    void Awake()
     {
-        agents = GameObject.FindObjectsOfType<NavMeshAgent>();
-		InitGame ();
+        agentsManager = GameObject.Find("ManagerNavMesh").GetComponent<AgentsManager>();
+        navAgent = transform.GetComponent<NavMeshAgent>();
+        myFocusEtatAgent = CibleAgent.nothing;
     }
 
-	public void InitGame(){
-		int nbAgents = agents.Length;
-
-		int nbLawPv = (int)Mathf.Round(nbAgents * pourcentLowPV/100);
-		int nbMaxPv = (int)Mathf.Round(nbAgents * pourcentMaxPV/100);
-		int nbLead = (int)Mathf.Round(nbAgents * pourcentLead/100);
-
-
-		for (int i = 0; i < nbLawPv; i++){	agents [i].GetComponent<AgentInfo> ().SetFocusPlayer (player [0]);}
-		for (int i = nbLawPv; i < nbLawPv+nbMaxPv; i++){	agents [i].GetComponent<AgentInfo> ().SetFocusPlayer (player [1]);}
-		for (int i = nbLawPv+nbMaxPv; i < nbLawPv+nbMaxPv+nbLead; i++){		agents [i].GetComponent<AgentInfo> ().SetFocusPlayer (player [2]);}
-		for (int i = nbLawPv+nbMaxPv+nbLead; i < nbAgents; i++){		agents [i].GetComponent<AgentInfo> ().SetFocusPlayer (player [3]);}
-	}
-
-	public void ChecLawPlayer(){
-	}
-		
-    private void Update()
+    void Update()
     {
-		timeAgent += Time.deltaTime;
-		if ( timeAgent > timeLeftAgentLook)
-		{
-			for (int i = 0; i < agents.Length; i++)
-			{
-				float distance = Mathf.Abs (Vector3.Distance (agents [i].transform.position, player[0].transform.position));
-				if(distance>maxPosPlayer){
-					float randStopDist = Random.Range (15, 25);
-					agents[i].stoppingDistance = randStopDist;
-					agents[i].SetDestination(player[0].transform.position);
-				}
-			}
-			timeAgent = 0;
-		}
+        if (dead)
+        {
+            DeadFonction();
+            dead = false;
+        }
     }
+
+    public void TargetPlayer(float stopDistance, float maxdist)
+    {
+        float distance = Mathf.Abs(Vector3.Distance(transform.position, myFocusPlayer.transform.position));
+        navAgent.stoppingDistance = stopDistance;
+        if (distance > maxdist)
+        {
+            navAgent.SetDestination(myFocusPlayer.transform.position);
+        }
+    }
+
+    #region ChangeEtat
+    public void DeadFonction()
+    {
+        agentsManager.DeadAgent(myFocusEtatAgent.ToString(), this.gameObject);
+    }
+
+    public void SetFocusLawPlayer(GameObject player)
+    {
+        myFocusEtatAgent = CibleAgent.lawPlayer;
+        myFocusPlayer = player;
+    }
+
+    public void SetFocusMaxPlayer(GameObject player)
+    {
+        myFocusEtatAgent = CibleAgent.maxPlayer;
+        myFocusPlayer = player;
+    }
+
+    public void SetFocusLeadPlayer(GameObject player)
+    {
+        myFocusEtatAgent = CibleAgent.leadPlayer;
+        myFocusPlayer = player;
+    }
+
+    public void SetFocusRandomPlayer(GameObject player)
+    {
+        myFocusEtatAgent = CibleAgent.randomPlayer;
+        myFocusPlayer = player;
+    }
+    #endregion
 }
---
+++ Assets/Scripts/NavMesh/AgentsManager.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentsManager : MonoBehaviour
+{
+
+    /// <summary> Public <summary>
+    [Header("-------------------")]
+    [Header("----INFO PLAYER----")]
+    [Header("-------------------")]
+
+    [Header("PlayerPref")]
+    public GameObject[] player;
+
+    [Header("Timer Agent Sleep and Info")]
+    public float maxPosPlayer;
+    public float timeLeftAgentLook = 0.5f;
+
+    [Header("------------------")]
+    [Header("----INFO FOCUS----")]
+    [Header("------------------")]
+    public int pourcentLowPV;
+    public int pourcentMaxPV;
+    public int pourcentLead;
+
+    [Header("------------------")]
+    [Header("----INFO AGENT----")]
+    [Header("------------------")]
+
+    [Header("List Agent Focus Law")]
+    public List<AgentController> lawpvAgents;
+
+    [Header("List Agent Focus Max")]
+    public List<AgentController> maxpvAgents;
+
+    [Header("List Agent Focus Lead")]
+    public List<AgentController> leadAgents;
+
+    [Header("List Agent Focus Other")]
+    public List<AgentController> othersAgents;
+
+
+    /// <summary> Private <summary>
+
+    //Agent
+    private AgentController[] agents;
+    private int nbAgents;
+    private int nbLawPv;
+    private int nbMaxPv;
+    private int nbLead;
+
+    //AgentFocus;
+    private GameObject playerLaw;
+    private GameObject playerMax;
+    private GameObject playerLead;
+
+    //Timer
+    private float timeAgent = 5;
+
+    void Start()
+    {
+        playerLaw = player[0].gameObject;
+        playerMax = player[0].gameObject;
+        playerLead = player[0].gameObject;
+        agents = GameObject.FindObjectsOfType<AgentController>();
+        InitGame();
+    }
+    private void Update()
+    {
+
+        timeAgent += Time.deltaTime;
+        if (timeAgent > timeLeftAgentLook)
+        {
+            for (int i = 0; i < agents.Length; i++)
+            {
+                float randStopDist = Random.Range(10, 20);
+                agents[i].TargetPlayer(randStopDist, maxPosPlayer);
+            }
+            timeAgent = 0;
+        }
+    }
+
+    #region WhoFocus
+    public void ChangeEtatFocus(GameObject lawP, GameObject maxP, GameObject leadP)
+    {
+        playerLaw = lawP;
+        playerMax = maxP;
+        playerLead = leadP;
+    }
+    #endregion
+
+    #region IniGame
+    public void InitGame()
+    {
+        nbAgents = agents.Length;
+
+        nbLawPv = (int)Mathf.Round(nbAgents * pourcentLowPV / 100);
+        nbMaxPv = (int)Mathf.Round(nbAgents * pourcentMaxPV / 100);
+        nbLead = (int)Mathf.Round(nbAgents * pourcentLead / 100);
+
+        for (int i = 0; i < agents.Length; i++)
+        {
+            int randomPlayer = Random.Range(0, player.Length);
+            othersAgents.Add(agents[i]);
+            agents[i].SetFocusRandomPlayer(player[randomPlayer]);
+        }
+
+        CheckFocusIni();
+    }
+    #endregion
+
+    #region Check if you need Agent
+    public void CheckFocusIni()
+    {
+        if (lawpvAgents.Count < nbLawPv)
+        {
+            int needAgent = nbLawPv - lawpvAgents.Count;
+            for (int i = 0; i < needAgent; i++)
+            {
+                lawpvAgents.Add(othersAgents[othersAgents.Count - 1]);
+                othersAgents[othersAgents.Count - 1].SetFocusLawPlayer(playerLaw);
+                othersAgents.Remove(othersAgents[othersAgents.Count - 1]);
+            }
+        }
+        if (maxpvAgents.Count < nbMaxPv)
+        {
+            int needAgent = nbMaxPv - maxpvAgents.Count;
+            for (int i = 0; i < needAgent; i++)
+            {
+                maxpvAgents.Add(othersAgents[othersAgents.Count - 1]);
+                othersAgents[othersAgents.Count - 1].SetFocusMaxPlayer(playerMax);
+                othersAgents.Remove(othersAgents[othersAgents.Count - 1]);
+            }
+        }
+        if (leadAgents.Count < nbLead)
+        {
+            int needAgent = nbLead - leadAgents.Count;
+            for (int i = 0; i < needAgent; i++)
+            {
+                leadAgents.Add(othersAgents[othersAgents.Count - 1]);
+                othersAgents[othersAgents.Count - 1].SetFocusLeadPlayer(playerLead);
+                othersAgents.Remove(othersAgents[othersAgents.Count - 1]);
+            }
+        }
+    }
+    #endregion
+
+    #region Check if dead Agent
+    public void DeadAgent(string etatAgentDead, GameObject agentDead)
+    {
+        int randomPlayer = Random.Range(0, player.Length);
+        if (etatAgentDead == "lawPlayer")
+        {
+            for (int i = 0; i < lawpvAgents.Count; i++)
+            {
+                if (lawpvAgents[i].gameObject == agentDead)
+                {
+                    othersAgents.Insert(0, lawpvAgents[i]);
+                    othersAgents[i].SetFocusRandomPlayer(player[randomPlayer]);
+                    lawpvAgents.Remove(lawpvAgents[i]);
+                    break;
+                }
+            }
+        }
+        else if (etatAgentDead == "maxPlayer")
+        {
+            for (int i = 0; i < maxpvAgents.Count; i++)
+            {
+                if (maxpvAgents[i].gameObject == agentDead)
+                {
+                    othersAgents.Insert(0, maxpvAgents[i]);
+                    maxpvAgents[i].SetFocusRandomPlayer(player[randomPlayer]);
+                    maxpvAgents.Remove(maxpvAgents[i]);
+                    break;
+                }
+            }
+        }
+        else if (etatAgentDead == "leadPlayer")
+        {
+            for (int i = 0; i < leadAgents.Count; i++)
+            {
+                if (leadAgents[i].gameObject == agentDead)
+                {
+                    othersAgents.Insert(0, leadAgents[i]);
+                    leadAgents[i].SetFocusRandomPlayer(player[randomPlayer]);
+                    leadAgents.Remove(leadAgents[i]);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Je suis Random");
+        }
+        CheckFocusIni();
+    }
+    #endregion
+
+}
