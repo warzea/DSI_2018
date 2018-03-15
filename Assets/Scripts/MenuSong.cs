4bd36924ad69927e8753c7426da8445659bb7010
Alexandre <alexndre.re@gmail.com>
2018-03-15 11:33:41 +0100

    WIP_Song

---
+++ Assets/Scripts/MenuSong.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MenuSong : MonoBehaviour
+{
+	#region Variables
+	public AudioManager ThisAudM;
+	#endregion
+
+	#region Mono
+	void Awake ()
+	{
+		ThisAudM = GetComponent<AudioManager> ();
+		ThisAudM.Initialize ();
+	}
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+	#endregion
+
+}
