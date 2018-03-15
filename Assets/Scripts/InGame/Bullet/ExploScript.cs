fc512d556d1d03e831d320afecc910f286009de6
Unknown <davidbourel.29@hotmail.fr>
2018-03-15 16:14:21 +0100

    WIP_Weapon

--- Assets/Scripts/InGame/Bullet/ExploScript.cs
+++ Assets/Scripts/InGame/Bullet/ExploScript.cs
@@ -2,42 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExploScript : MonoBehaviour 
+public class ExploScript : MonoBehaviour
 {
-	#region Variables
+    #region Variables
     public GameObject GetEffect;
-	public float ScaleExplo;
+    public float ScaleExplo;
 
-	public float TimeStay;
-	public float TimeEffect;
-	float currScale = 0;
-	#endregion
-	
-	#region Mono
-	void Start ( )
-	{
-		if ( TimeStay == 0 )
-		{
-			TimeStay = TimeEffect;
-		}
-		
-		transform.localScale = new Vector3(ScaleExplo, ScaleExplo, ScaleExplo);
+    public float TimeStay;
+    public float TimeEffect;
+    float currScale = 0;
+    #endregion
 
-		if ( GetEffect != null )
-		{
-			Destroy ( (GameObject) Instantiate(GetEffect, transform.position, Quaternion.identity), TimeEffect );
-		}
-		
-		Destroy ( gameObject, TimeStay );
-	}
-	#endregion
-	
-	#region Public Methods
-	
-	#endregion
+    #region Mono
+    void Start()
+    {
+        if (TimeStay == 0)
+        {
+            TimeStay = TimeEffect;
+        }
 
-	#region Private Methods
+        transform.localScale = new Vector3(ScaleExplo, ScaleExplo, ScaleExplo);
 
-	#endregion
+        if (GetEffect != null)
+        {
+            GameObject exploEffect = (GameObject)Instantiate(GetEffect, transform.position, Quaternion.identity);
+            Destroy(exploEffect, TimeEffect);
+        }
+
+        Destroy(gameObject, TimeStay);
+    }
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
 
 }
